2961b4ff49a0476ec0d1c50e2ed8ceb3ebacd3ff
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-10-19 13:09:47 +0530

    contextualbench-remaining-datasets

--- contrastive_activation_addition/script_contextualbencheval.sh
+++ contrastive_activation_addition/script_contextualbencheval.sh
@@ -5,3 +5,13 @@
 python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 1 2 3 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=multihopqa.json"
 python scoring.py --behaviors "context-focus"
 python average_scores.py
+
+# REMAINING DATASETS
+# HOTPOT-QA
+python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 1 2 3 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=hotpotqa.json"
+# TRIVIA-QA
+python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 1 2 3 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=triviaqa.json"
+# TRUTHFUL-QA
+python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 1 2 3 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=truthfulqa.json"
+python scoring.py --behaviors "context-focus"
+python average_scores.py
