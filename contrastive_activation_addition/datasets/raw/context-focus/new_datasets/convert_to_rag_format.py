9e92fbd9fae2ecff2faad924e22bda7885f5faff
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-10-20 13:40:53 +0530

    Added more datasets

---
+++ contrastive_activation_addition/datasets/raw/context-focus/new_datasets/convert_to_rag_format.py
@@ -0,0 +1,29 @@
+import json
+
+final_list = []
+
+with open("multihopqa.json",'r') as f:
+    li = json.load(f)
+    rows = li[0]['rows']
+
+    for row in rows:
+        context = "[Document 1]: "
+        contexts = row['row']['context']['content']
+
+        for i,item in enumerate(contexts):
+            for sentence in item:
+                if not context == "":
+                    context = context + " "
+                context = context + sentence
+            
+            if not i==len(contexts)-1:
+                context = context + f"\n\n[Document {i+2}]: "
+
+        question = row['row']["question"]
+        system = "You are a helpful assistant. Based on the following documents, provide a concise answer. Your answer should be short, containing only the essential information."
+        combined = f"{system}\n\n{context}\n\nQuestion: {question}"
+
+        final_list.append({"question": combined, "answer": row['row']['answer']})
+
+with open("rag_format_multihopqa.json",'w') as f:
+    json.dump(final_list,f)
