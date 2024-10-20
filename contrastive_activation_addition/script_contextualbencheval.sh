2b9e6d98acb7519fb8f2120394e9d8f3ba617942
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-10-20 13:41:44 +0530

    hi

--- contrastive_activation_addition/script_contextualbencheval.sh
+++ contrastive_activation_addition/script_contextualbencheval.sh
@@ -16,3 +16,8 @@ python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED
 
 python scoring.py --behaviors "context-focus"
 python average_scores.py
+
+# TESTING RAG_MULTIHOP_QA WITH VANILLA LLAMA-3.1-8B TO COMPARE RESULTS WITH PAPER
+# ASSUMING STANDARD RAG FORMAT IS FOLLOWED AND WILL EVALUATE USING EXACT-MATCH
+# RAG-MULTIHOP-QA
+python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=ragmultihopqa.json"
