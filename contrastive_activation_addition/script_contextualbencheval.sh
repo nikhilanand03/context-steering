9d2597a60e41d93c65e6376e193352301d651579
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-10-21 11:18:27 +0530

    Committing

--- contrastive_activation_addition/script_contextualbencheval.sh
+++ contrastive_activation_addition/script_contextualbencheval.sh
@@ -21,3 +21,7 @@ python average_scores.py
 # ASSUMING STANDARD RAG FORMAT IS FOLLOWED AND WILL EVALUATE USING EXACT-MATCH
 # RAG-MULTIHOP-QA
 python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=ragmultihopqa.json"
+
+# TESTING MUSIQUE DATASET WITH THE SUPPORTING CONTEXTS (SHORT CONTEXTS)
+# TO VERIFY THAT LONG CONTEXTS IS THE REASON STEERING IS FAILING
+python prompting_with_steering.py --layers 12 --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_12_Meta-Llama-3.1-8B-Instruct.pt" --use_latest --multipliers 0 1 2 3 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=musique.json"
