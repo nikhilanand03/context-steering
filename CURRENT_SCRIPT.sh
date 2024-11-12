f071fb71f4b0f1835e9bcfcff4a1c8889d63ce72
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-11-13 00:35:17 +0530

    hi

--- CURRENT_SCRIPT.sh
+++ CURRENT_SCRIPT.sh
@@ -1,5 +1,14 @@
-git pull
-cd contrastive_activation_addition/datasets/raw/context-focus/new_datasets/generate_datasets
+# git pull
+# cd contrastive_activation_addition/datasets/raw/context-focus/new_datasets/generate_datasets
 
-chmod +x get_hotpot_outputs.sh
-./get_hotpot_outputs.sh
+# chmod +x get_hotpot_outputs.sh
+# ./get_hotpot_outputs.sh
+
+
+############################################
+
+git pull
+cd contrastive_activation_addition
+
+chmod +x script_FULL_multicontext_llama_8b.sh
+./script_FULL_multicontext_llama_8b.sh
