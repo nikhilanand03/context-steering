feaab5daac7b9cf398eddb53811ed14952e091bc
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-11-13 12:13:59 +0530

    stuff

--- CURRENT_SCRIPT.sh
+++ CURRENT_SCRIPT.sh
@@ -1,16 +1,19 @@
-# git pull
+# 1. HOTPOT OUTPUTS
+
 # cd contrastive_activation_addition/datasets/raw/context-focus/new_datasets/generate_datasets
 
 # chmod +x get_hotpot_outputs.sh
 # ./get_hotpot_outputs.sh
 
-
 ############################################
 
-git pull
+# 2. MULTI_CONTEXT
+
 cd contrastive_activation_addition
 
 chmod +x script_FULL_multicontext_llama_8b.sh
 ./script_FULL_multicontext_llama_8b.sh
 
-# OUTPUTS? DIR: contrastive_activation_addition; FOLDERS: analysis, normalized_vectors, results, 
+# OUTPUTS? DIR: contrastive_activation_addition; FOLDERS: analysis, normalized_vectors, results, vectors
+
+############################################
