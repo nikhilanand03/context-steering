09e1a6fd2c49f32404ab2dd0ff25c65dbdde8089
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-11-18 17:48:58 +0530

    hi

--- contrastive_activation_addition/script_FULL_multicontext_llama_8b.sh
+++ contrastive_activation_addition/script_FULL_multicontext_llama_8b.sh
@@ -3,23 +3,23 @@
 
 ############################### 1 ####################################
 
-rm -r analysis
-rm -r results
-rm -r normalized_vectors
-rm -r vectors
+# rm -r analysis
+# rm -r results
+# rm -r normalized_vectors
+# rm -r vectors
 
 ## < ---------------------------------------------------------------->
 
 # Train vecs using "generate_dataset_long_contexts_dummy.json"
 # (I changed generate_vectors to allow multicontext and tokenize differently in the case of multicontext)
-python generate_vectors.py --layers $(seq 0 31) --save_activations --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_multicontext_2500.json" --multicontext
-python normalize_vectors.py --use_latest
-python plot_activations.py --layers $(seq 0 31) --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_multicontext_2500.json"
+# python generate_vectors.py --layers $(seq 0 31) --save_activations --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_multicontext_2500.json" --multicontext
+# python normalize_vectors.py --use_latest
+# python plot_activations.py --layers $(seq 0 31) --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_multicontext_2500.json"
 
 # TESTING ON MCQ DATASET TO GET BEST LAYER 
 # (I changed prompt with steering to account for multicontext in the type="ab" case here)
-python prompting_with_steering.py --layers $(seq 0 31) --use_latest --multipliers -1 0 1 --type ab --behaviors "context-focus" --override_ab_dataset "test_dataset_ab_multicontext_2500.json" --multicontext
-python plot_results.py --layers $(seq 0 31) --multipliers -1 1 --type ab --behaviors "context-focus"
+# python prompting_with_steering.py --layers $(seq 0 31) --use_latest --multipliers -1 0 1 --type ab --behaviors "context-focus" --override_ab_dataset "test_dataset_ab_multicontext_2500.json" --multicontext
+# python plot_results.py --layers $(seq 0 31) --multipliers -1 1 --type ab --behaviors "context-focus"
 
 ############################### 1 ####################################
 
@@ -30,22 +30,22 @@ python plot_results.py --layers $(seq 0 31) --multipliers -1 1 --type ab --behav
 
 ############################### 2 ####################################
 
-# BESTLAYER=14 ## GET BEST LAYER HERE
+BESTLAYER=14 ## GET BEST LAYER HERE
 
 # MULTIPLE MULTIPLIER TESTING ON MCQ DATASET
 
-# python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 0 1 2 3 4 5 6 --type ab --behaviors "context-focus" --override_ab_dataset "test_dataset_ab_multicontext_2500.json" --multicontext
-# python plot_results.py --layers $BESTLAYER --multipliers 0 1 2 3 4 5 6 --type ab --behaviors "context-focus"
+python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 0 1 2 3 4 5 6 --type ab --behaviors "context-focus" --override_ab_dataset "test_dataset_ab_multicontext_2500.json" --multicontext
+python plot_results.py --layers $BESTLAYER --multipliers 0 1 2 3 4 5 6 --type ab --behaviors "context-focus"
 
 # OPEN-ENDED TESTING (multicontext hotpotQA dataset)
 
-# python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 0 1 2 3 4 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=hotpotqa_multicontext.json" --multicontext
+python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 0 1 2 3 4 5 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=hotpotqa_multicontext.json" --multicontext
 # python scoring.py --behaviors "context-focus" --multicontext
 # python average_scores.py
 
 # OPEN-ENDED TESTING (FAILURES DS)
 
-# python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 2 3 4 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=hotpotqa_multicontext_failures_625.json" --multicontext
+python prompting_with_steering.py --layers $BESTLAYER --use_latest --multipliers 0 1 2 3 4 5 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_version=hotpotqa_multicontext_failures_625.json" --multicontext
 # python scoring.py --behaviors "context-focus" --multicontext
 # python average_scores.py
 
