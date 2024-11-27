cad8398cf1706875397517936f47858f6f43d3b7
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-11-27 11:00:29 +0530

    hi

--- contrastive_activation_addition/script_FULL_no_options.sh
+++ contrastive_activation_addition/script_FULL_no_options.sh
@@ -3,21 +3,21 @@
 
 ############## STEP 1: generate_vectors #############
 
-rm -r analysis
-rm -r results
-rm -r normalized_vectors
-rm -r vectors
-rm -r activations
+# rm -r analysis
+# rm -r results
+# rm -r normalized_vectors
+# rm -r vectors
+# rm -r activations
 
-python generate_vectors.py --layers $(seq 0 31) --save_activations --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_no_options.json" --no_options
-python normalize_vectors.py --use_latest
+# python generate_vectors.py --layers $(seq 0 31) --save_activations --use_latest --behaviors "context-focus" --override_dataset "generate_dataset_no_options.json" --no_options
+# python normalize_vectors.py --use_latest
 
 # Here, plot_activations.py will plot the activations of the positive and negative classes (no A/B distinction)
 python plot_activations.py --layers $(seq 0 31) --use_latest --behaviors "context-focus" --no_options
 
 # I create a dataset called "test_dataset_oe_best_layer_no_options.json"
 # This uses same examples as test_dataset_ab_multi_context_2500.json except has no options!!
-python prompting_with_steering.py --layers $(seq 0 31) --use_latest --multipliers -2 -1 0 1 2 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_oe_best_layer_no_options.json" --no_options
+# python prompting_with_steering.py --layers $(seq 0 31) --use_latest --multipliers -2 -1 0 1 2 --type open_ended --behaviors "context-focus" --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_oe_best_layer_no_options.json" --no_options
 
 ############## STEP 2: we run this locally and then get the best layer using the scores ##############
 
