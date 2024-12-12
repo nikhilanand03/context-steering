c1e686715cd915d9d7fa741203eab37996bd7513
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-12-12 22:42:08 +0530

    hi

---
+++ CURRENT_SCRIPT2.sh
@@ -0,0 +1,10 @@
+# 1. QuoteSum dataset! Using no-options vector to generate.
+
+cd contrastive_activation_addition
+
+chmod +x script_llama318b_quotesum_from_nqswap.sh
+./script_llama318b_quotesum_from_nqswap.sh
+
+tar -czvf all_results.tar.gz ./results_quotesum
+
+#############################################################
