0bb78b11979839ca84cb8e97c57247dba95b863c
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-12-13 13:59:04 +0530

    hi

--- contrastive_activation_addition/script_sys_prompt_nq_swap.sh
+++ contrastive_activation_addition/script_sys_prompt_nq_swap.sh
@@ -2,44 +2,46 @@
 
 # llama-3.1-8b
 
-rm -r analysis
-rm -r results
-rm -r normalized_vectors
-rm -r vectors
-rm -r activations
+# rm -r analysis
+# rm -r results
+# rm -r normalized_vectors
+# rm -r vectors
+# rm -r activations
 
-LAYER=12
+# LAYER=12
 
-python prompting_with_steering.py \
-    --layers $LAYER \
-    --use_latest \
-    --system_prompt "pos" \
-    --multipliers -2 -1 0 1 2 3 \
-    --type open_ended \
-    --behaviors "context-focus" \
-    --suffix "_llama8b" \
-    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_gpt_cleaning_750_cqformat.json" \
-    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_${LAYER}_Meta-Llama-3.1-8B-Instruct.pt"
+# python prompting_with_steering.py \
+#     --layers $LAYER \
+#     --use_latest \
+#     --system_prompt "pos" \
+#     --multipliers -2 -1 0 1 2 3 \
+#     --type open_ended \
+#     --behaviors "context-focus" \
+#     --suffix "_llama8b" \
+#     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_gpt_cleaning_750_cqformat.json" \
+#     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_${LAYER}_Meta-Llama-3.1-8B-Instruct.pt"
 
 # python scoring.py --behaviors "context-focus"; python average_scores.py
 
 # mistral-7b
 # GO TO helpers.py AND MAKE SURE THE MISTRAL_LIKE_MODEL IS SET TO "MISTRAL-7B"
 
-LAYER=13
+# LAYER=13
 
-python prompting_with_steering.py \
-    --layers $LAYER \
-    --use_mistral \
-    --system_prompt "pos" \
-    --multipliers -2 -1 0 1 2 3 4 \
-    --type open_ended \
-    --behaviors "context-focus" \
-    --suffix "_mistral7b" \
-    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_gpt_cleaning_750_cqformat.json" \
-    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt"
+# python prompting_with_steering.py \
+#     --layers $LAYER \
+#     --use_mistral \
+#     --system_prompt "pos" \
+#     --multipliers -2 -1 0 1 2 3 4 \
+#     --type open_ended \
+#     --behaviors "context-focus" \
+#     --suffix "_mistral7b" \
+#     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_gpt_cleaning_750_cqformat.json" \
+#     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt"
 
 # python scoring.py --behaviors "context-focus"; python average_scores.py
+
+############ PART 2 (Need to change mistral-like-model before running this) ############
 
 # llama-70b
 # GO TO helpers.py AND MAKE SURE THE MISTRAL_LIKE_MODEL IS SET TO "LLAMA-70B"
