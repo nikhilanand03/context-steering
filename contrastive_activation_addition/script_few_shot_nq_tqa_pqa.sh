b3796ab3baa52227b4fd4b7a8b066f03111bb8ae
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-02 16:33:06 +0530

    hi

---
+++ contrastive_activation_addition/script_few_shot_nq_tqa_pqa.sh
@@ -0,0 +1,16 @@
+# NQ dataset, Mistral 7B Few-shot prompting
+
+LAYER=13
+
+python prompting_with_steering.py \
+    --layers $LAYER \
+    --use_latest \
+    --multipliers -2 -1 0 1 2 3 \
+    --type open_ended \
+    --behaviors "context-focus" \
+    --suffix "_mistral7b_NQ" \
+    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/nq_test.tsv" \
+    --few_shot \
+    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt"
+
+############################################
