5649e4738c3a6ae0b717b3d0475912c105aa15a9
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-10 03:57:49 +0530

    added debugging step in nqswap script

--- ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_nqswap_cad_cd_regcls.sh
+++ ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_nqswap_cad_cd_regcls.sh
@@ -1,4 +1,21 @@
 ##################################################
+# Run NQ-SWAP on Reg-Cls (DEBUGGING)
+
+CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
+ --model_name mistralai/Mistral-7B-Instruct-v0.3 \
+ --input_file ../data/nqswap/nq_swap_test.tsv \
+ --eval_method vanilla \
+ --n_examples 5 \
+ --use_gold_ctx \
+ --use_random_irr \
+ --bf16 \
+ --alias 'nq-alpha-0.5' \
+ --suffix '_regcls_nqswap_DEBUG' \
+ --debug
+
+##################################################
+
+##################################################
 # Run NQ-SWAP on Reg-Cls
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
