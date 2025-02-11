e1264d3ebb99cb14ba2d575763fcecd6f1b1a64f
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-11 20:31:30 +0530

    changed popqa script

--- ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_popqa_cad_cd_regcls.sh
+++ ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_popqa_cad_cd_regcls.sh
@@ -3,14 +3,14 @@
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
  --model_name mistralai/Mistral-7B-Instruct-v0.3 \
- --input_file ../data/nqswap/nq_swap_test.tsv \
+ --input_file ../data/popqa/popqa_test.tsv \
+ --ret_path ./data/popqa/popqa_contriever_results.jsonl \
  --eval_method vanilla \
  --n_examples 5 \
- --use_gold_ctx \
  --use_random_irr \
  --bf16 \
  --alias 'nq-alpha-0.5' \
- --suffix '_regcls_nqswap_DEBUG' \
+ --suffix '_regcls_popqa_DEBUG' \
  --debug
 
 ##################################################
@@ -20,10 +20,10 @@ CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
  --model_name mistralai/Mistral-7B-Instruct-v0.3 \
- --input_file ../data/nqswap/nq_swap_test.tsv \
+ --input_file ../data/popqa/popqa_test.tsv \
+ --ret_path ./data/popqa/popqa_contriever_results.jsonl \
  --eval_method vanilla \
  --n_examples 5 \
- --use_gold_ctx \
  --use_random_irr \
  --bf16 \
  --alias 'nq-alpha-0.5' \
@@ -36,10 +36,10 @@ CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
  --model_name mistralai/Mistral-7B-Instruct-v0.3 \
- --input_file ../data/nqswap/nq_swap_test.tsv \
+ --input_file ../data/popqa/popqa_test.tsv \
+ --ret_path ./data/popqa/popqa_contriever_results.jsonl \
  --eval_method CD \
  --n_examples 5 \
- --use_gold_ctx \
  --use_random_irr \
  --bf16 \
  --alpha 0.5 \
@@ -53,7 +53,8 @@ CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
  --model_name mistralai/Mistral-7B-Instruct-v0.3 \
- --input_file ../data/nqswap/nq_swap_test.tsv \
+ --input_file ../data/popqa/popqa_test.tsv \
+ --ret_path ./data/popqa/popqa_contriever_results.jsonl \
  --eval_method CAD \
  --n_examples 5 \
  --use_gold_ctx \
