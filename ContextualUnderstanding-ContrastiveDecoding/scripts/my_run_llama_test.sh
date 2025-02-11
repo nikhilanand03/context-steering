05486fba838c364d26d861813d8c26130bb16714
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-12 04:30:27 +0530

    test llama

--- ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_llama_test.sh
+++ ContextualUnderstanding-ContrastiveDecoding/scripts/my_run_llama_test.sh
@@ -2,7 +2,7 @@
 # run experiment with CAD decoding (with context) (Llama-8B)
 
 CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
- --model_name meta-llama/Meta-Llama-3.1-8B-Instruct \
+ --model_name meta-llama/Llama-2-13b-chat-hf \
  --input_file ../data/nq/nq_test.tsv \
  --eval_method CAD \
  --n_examples 5 \
@@ -11,5 +11,6 @@ CUDA_VISIBLE_DEVICES=0 python ../src/contrastive_decoding/run_qa_prompt.py \
  --bf16 \
  --alpha 0.5 \
  --alias 'nq-alpha-0.5'
+#  --model_name meta-llama/Meta-Llama-3.1-8B-Instruct \
 
 ##################################################
