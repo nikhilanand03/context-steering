2ff83079b52c011a1235543b01c1eeb8c14e6ec8
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-14 18:28:46 +0530

    finished writing fs steering script

--- contrastive_activation_addition/script_NEW_fewshot.sh
+++ contrastive_activation_addition/script_NEW_fewshot.sh
@@ -14,3 +14,54 @@ python prompting_with_steering.py \
     --max_fs_length 15000
 
 #############################
+
+LAYER=13
+
+python prompting_with_steering.py \
+    --layers $LAYER \
+    --use_mistral \
+    --multipliers 0 2 \
+    --type open_ended \
+    --behaviors "context-focus" \
+    --suffix "_mistral7b_TQA_FS" \
+    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_triviaqa.json" \
+    --few_shot \
+    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
+    --sample 1000 \
+    --max_fs_length 15000
+
+#############################
+
+LAYER=13
+
+python prompting_with_steering.py \
+    --layers $LAYER \
+    --use_mistral \
+    --multipliers 0 2 \
+    --type open_ended \
+    --behaviors "context-focus" \
+    --suffix "_mistral7b_PQA_Contr_FS" \
+    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_popqa_contriever.json" \
+    --few_shot \
+    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
+    --sample 1000 \
+    --max_fs_length 15000
+
+#############################
+
+LAYER=13
+
+python prompting_with_steering.py \
+    --layers $LAYER \
+    --use_mistral \
+    --multipliers 0 2 \
+    --type open_ended \
+    --behaviors "context-focus" \
+    --suffix "_mistral7b_NQswap_FS" \
+    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_nqswap.json" \
+    --few_shot \
+    --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
+    --sample 1000 \
+    --max_fs_length 15000
+
+#############################
