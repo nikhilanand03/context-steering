05fd9b69db7dc7de5a4bdd19dcbd9c37600381e1
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-02-15 15:40:08 +0530

    modified script for multipliers 1 and 3

--- contrastive_activation_addition/script_NEW_fewshot.sh
+++ contrastive_activation_addition/script_NEW_fewshot.sh
@@ -3,10 +3,10 @@ LAYER=13
 python prompting_with_steering.py \
     --layers $LAYER \
     --use_mistral \
-    --multipliers 0 2 \
+    --multipliers 1 3 \
     --type open_ended \
     --behaviors "context-focus" \
-    --suffix "_mistral7b_NQ_FS" \
+    --suffix "_mistral7b_NQ_FS_M=13" \
     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_nq.json" \
     --few_shot \
     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
@@ -20,10 +20,10 @@ LAYER=13
 python prompting_with_steering.py \
     --layers $LAYER \
     --use_mistral \
-    --multipliers 0 2 \
+    --multipliers 1 3 \
     --type open_ended \
     --behaviors "context-focus" \
-    --suffix "_mistral7b_TQA_FS" \
+    --suffix "_mistral7b_TQA_FS_M=13" \
     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_triviaqa.json" \
     --few_shot \
     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
@@ -37,10 +37,10 @@ LAYER=13
 python prompting_with_steering.py \
     --layers $LAYER \
     --use_mistral \
-    --multipliers 0 2 \
+    --multipliers 1 3 \
     --type open_ended \
     --behaviors "context-focus" \
-    --suffix "_mistral7b_PQA_Contr_FS" \
+    --suffix "_mistral7b_PQA_Contr_FS_M=13" \
     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_popqa_contriever.json" \
     --few_shot \
     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
@@ -54,11 +54,11 @@ LAYER=13
 python prompting_with_steering.py \
     --layers $LAYER \
     --use_mistral \
-    --multipliers 0 2 \
+    --multipliers 1 3 \
     --type open_ended \
     --behaviors "context-focus" \
-    --suffix "_mistral7b_NQswap_FS" \
-    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_nqswap.json" \
+    --suffix "_mistral7b_NQswap_FS_M=13" \
+    --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_open_ended_new_nq_swap.json" \
     --few_shot \
     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_mistral-7b-0.3/normalized_vectors/context-focus/vec_layer_${LAYER}_Mistral-7B-Instruct-v0.3.pt" \
     --sample 1000 \
