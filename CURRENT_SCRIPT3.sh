381563e4dcb737f0870e67262bbc7c365c4d974e
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2025-05-17 10:54:01 +0530

    made 1000 samples

--- CURRENT_SCRIPT3.sh
+++ CURRENT_SCRIPT3.sh
@@ -36,7 +36,7 @@ python prompting_with_steering.py \
     --confiqa \
     --behaviors "context-focus" \
     --suffix "_llama8b_confiqa_MC_1000" \
-    --sample 4 \
+    --sample 1000 \
     --override_oe_dataset_path "datasets/test/context-focus/test_dataset_varieties/test_dataset_oe_ConFiQA-MC.json" \
     --override_vector_path "1COMPLETED_RUNS/FULL_ANALYSIS_Llama-3.1-8b/normalized_vectors/context-focus/vec_layer_${LAYER}_Meta-Llama-3.1-8B-Instruct.pt"
 
